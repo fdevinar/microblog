47c2f4feeca9b37392749a4828f4b2aeff33963a
fdevinar <fc_devinar@hotmail.com>
2019-03-19 22:17:06 -0300

    end of chapter 13 - translation

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, flash, redirect, url_for, request
+from flask import render_template, flash, redirect, url_for, request, g
 from app import app, db
 from app.forms import LoginForm, RegistrationForm, EditProfileForm, PostForm
 from flask_login import current_user, login_user, logout_user, login_required
@@ -15,7 +15,7 @@ def before_request():
     if current_user.is_authenticated:
         current_user.last_seen = datetime.utcnow()
         db.session.commit()
-#    g.locale = str(get_locale())
+    g.locale = str(get_locale())
 
 
 @app.route('/', methods=['GET', 'POST'])
